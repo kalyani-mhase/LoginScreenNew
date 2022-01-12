3948d642f13509d3d4f6666fc7f92ac55ddd4553
kalyani2333 <91599782+kalyani2333@users.noreply.github.com>
2022-01-12 12:21:52 +0530

    Initial Commit

---
+++ LoginScreen2/ViewController.swift
@@ -0,0 +1,38 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+ 
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var facebookOutlet: UIButton!
+    
+    @IBOutlet weak var forgotOutlet: UIButton!
+    @IBOutlet weak var googleOutlet: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//       let emailLine = CALayer()
+//        let passwordLine = CALayer()
+//        emailLine.frame = CGRect(x: 0, y: emailText.frame.height, width: 320, height: 2)
+//        emailLine.backgroundColor = UIColor.init(red: 48/255, green: 48/225, blue: 99/255, alpha: 1).cgColor
+//        emailText.borderStyle = .none
+//        emailText.layer.addSublayer(emailLine)
+//        
+//        passwordLine.frame = CGRect(x: 0, y: passwordText.frame.height, width: 320, height: 2)
+//        passwordLine.backgroundColor = UIColor.init(red: 48/255, green: 48/225, blue: 99/255, alpha: 1).cgColor
+//        passwordText.borderStyle = .none
+//        passwordText.layer.addSublayer(passwordLine)
+//        facebookOutlet.layer.borderWidth = 3
+//        facebookOutlet.layer.borderColor = UIColor.systemBlue.cgColor
+//        googleOutlet.layer.borderWidth = 3
+//        googleOutlet.layer.borderColor = UIColor.systemRed.cgColor
+        
+    }
+
+
+}
+
---
+++ LoginScreen2UITests/LoginScreen2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoginScreen2UITestsLaunchTests.swift
+//  LoginScreen2UITests
+//
+//  Created by Admin on 05/01/22.
+//
+
+import XCTest
+
+class LoginScreen2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
